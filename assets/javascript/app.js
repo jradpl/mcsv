9b1b5f0297e4c969209217da1816a508ae1e4ae8
Radosław Jarzyński <r@jarzynski.dev>
2023-01-10 04:46:11 +0100

    Make form Twig

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,7 +6,10 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.scss';
+import '../styles/app.scss';
+
+//any JS
+import './bootstrap5'
 
 // start the Stimulus application
 import './bootstrap';
